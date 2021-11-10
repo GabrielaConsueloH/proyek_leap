2412764cab4e47bd795b1501076772cf3acc8c26
gaby205 <c14180055@john.petra.ac.id>
2021-11-10 23:42:16 +0700

    Login 10/11/2021

--- lib/Widgets/custom_text.dart
+++ lib/Widgets/custom_text.dart
@@ -3,43 +3,28 @@ import 'package:flutter/material.dart';
 class CustomText extends StatelessWidget {
   final String text;
   final double size;
-  final FontWeight fontweight ;
+  final FontWeight fontweight;
   final Color color;
   final double wordSpacing;
-  final VoidCallback? onClick;
 
   const CustomText({
-    this.size  = 18,
+    this.size = 18,
     required this.text,
     this.fontweight = FontWeight.w500,
     this.color = Colors.black,
     this.wordSpacing = 1,
-    this.onClick,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: onClick == null
-          ? Text(
-              text,
-              style: TextStyle(
-                  fontSize: size,
-                  fontWeight: fontweight,
-                  color: color,
-                  wordSpacing: wordSpacing),
-            )
-          : TextButton(
-              onPressed: () => onClick!.call(),
-              child: Text(
-                text,
-                style: TextStyle(
-                    fontSize: size,
-                    fontWeight: fontweight,
-                    color: color,
-                    wordSpacing: wordSpacing),
-              ),
-            ),
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+          fontSize: size,
+          fontWeight: fontweight,
+          color: color,
+          wordSpacing: wordSpacing),
     );
   }
 }
